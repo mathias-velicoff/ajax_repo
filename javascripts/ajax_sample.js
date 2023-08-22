5db2aec4a8d0a25f17d0819e027208c65596fae8
MathiasVlff <mathiasvelicofft@gmail.com>
2023-08-22 16:06:07 -0400

    actu2

--- javascripts/ajax_sample.js
+++ javascripts/ajax_sample.js
@@ -3,11 +3,13 @@ function getData() {
 	request.onreadystatechange = function () {
 		if (request.readyState == 4) {
 			if (request.status == 200) {
-				document.getElementById("title").innerHTML = request.response[0].title;
+				document.getElementById("title").innerHTML = request.response[0].title; //--modificación
 			}
 		}
 	};
-	request.open("GET", "ajax.json"); // --1
-	request.responseType = "text"; // --2
+	request.open("GET", "ajax.json");
+	request.responseType = "json";
 	request.send(null);
 }
+
+window.onload = getData;
