5a85d554d62a83606d0d5045f2fd44d62749362c
MathiasVlff <mathiasvelicofft@gmail.com>
2023-08-22 16:13:32 -0400

    actu3

--- javascripts/ajax_sample.js
+++ javascripts/ajax_sample.js
@@ -1,15 +1,26 @@
+let number = 0; //--1
+const titleArea = document.getElementById("title"); //--2
+const contentArea = document.getElementById("content"); //--2
+const videoArea = document.getElementById("video"); //--2
+const button = document.getElementById('btn'); //--3
+
 function getData() {
-	const request = new XMLHttpRequest();
-	request.onreadystatechange = function () {
-		if (request.readyState == 4) {
-			if (request.status == 200) {
-				document.getElementById("title").innerHTML = request.response[0].title; //--modificación
-			}
-		}
-	};
-	request.open("GET", "ajax.json");
-	request.responseType = "json";
-	request.send(null);
+  button.addEventListener('click', e => { //--4
+    const request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+      if (request.readyState == 4) {
+        if(request.status == 200) {
+          titleArea.innerHTML = request.response[number].title; //--5
+          contentArea.innerHTML = request.response[number].content; //--5
+          videoArea.setAttribute("src", request.response[number].url); //--6
+          number == 2 ? number = 0 : number++; //--7
+        }
+      }
+    }
+    request.open("GET", "ajax.json");
+    request.responseType = "json";
+    request.send(null);
+  })
 }
 
 window.onload = getData;
